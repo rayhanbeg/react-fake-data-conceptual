c0a41f124deda3a18146a19f08974cbd7d8d1a54
rayhanbeg <mdrayhan40301@gmail.com>
2024-03-15 12:36:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      
+      <div className="main-container">
+        <div className="cards-container">
+
+        </div>
+        <div className="card-container">
+          
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
