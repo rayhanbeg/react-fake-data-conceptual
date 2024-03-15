c7289869f0f0a0fa04fcc01ffe690ab38b37e327
rayhanbeg <mdrayhan40301@gmail.com>
2024-03-15 18:44:08 +0600

    added card name and price

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,67 @@
 
+import { useEffect } from 'react'
 import './App.css'
+import { useState } from 'react';
+import SingleProduct from './SingleProduct';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
+
+  useEffect(() => {
+    fetch('./fakeData.json')
+    .then(res => res.json())
+    .then(data => {
+      setProducts(data)
+    });
+  } ,[]);
+
+
+  const handleCart = (product) => {
+    
+    const isExist = cart.find(item => item.id === product.id);
+    // console.log(isExist);
+
+    if (!isExist) {
+      setCart([...cart, product])
+    }
+    else{
+      alert("Already exist")
+    }
+    
+  }
+// console.log(cart);
+// console.log(products);
   return (
     <div>
       
       <div className="main-container">
         <div className="cards-container">
-
+          {
+            products.map(product => <SingleProduct key={product.id} product = {product} handleCart = {handleCart}></SingleProduct>)
+          }
         </div>
         <div className="card-container">
-          
+         <h1> this is cart</h1>
+         <div className="cart-title">
+          <h5>name</h5>
+          <h5>price</h5>
+         </div>
+         <div>
+         {
+  cart.map((item, index) => (
+    <div key={index}>
+      
+      <div className='cart-info-div'>
+      <p>{index+1}</p>
+        <h5>{item.title.slice(0, 10)}</h5>
+        <h5>{item.price}</h5>
+      </div>
+    </div>
+  ))
+}
+         </div>
         </div>
       </div>
       
---
+++ src/SingleProduct.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+const SingleProduct = ({product, handleCart}) => {
+    // console.log(handleCart);
+    return (
+        <div>
+            <div className="cart">
+            <img className='cart-img' src={product.image} alt="" />
+            <h2>{product.title.slice(0, 10)}</h2>
+            <p>{product.description}</p>
+            <div className="cart-footer">
+              <h2>{product.price}$</h2>
+              <button onClick={() => handleCart(product)} className='add-btn'>add to cart</button>
+            </div>
+          </div>
+        </div>
+    );
+};
+
+SingleProduct.propTypes = {
+    product: PropTypes.object,
+    handleCart: PropTypes.func
+};
+  
+export default SingleProduct;
